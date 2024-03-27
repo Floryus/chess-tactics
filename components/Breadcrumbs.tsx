f4cf2a2b68039ce785db9150d297139bcc83920b
Marten Tietje <flyingpfirsich@gmail.com>
2024-03-27 18:18:20 +0100

    fix Breadcrumbs

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -1,25 +1,34 @@
 "use client";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
+interface BreadcrumbItem {
+  title: string;
+  href: string;
+}
 
 const Breadcrumbs = () => {
   const pathname = usePathname();
   const parts = pathname.split("/").filter((part) => part !== "");
-  parts.forEach((part, index) => {
-    parts[index] = part.charAt(0).toUpperCase() + part.slice(1);
-  });
+
+  const breadcrumbs: BreadcrumbItem[] = parts.map((part, index) => ({
+    title: part.charAt(0).toUpperCase() + part.slice(1),
+    href: index === 0 ? "/" : `/${parts.slice(0, index + 1).join("/")}`,
+  }));
 
   return (
     <nav className="pr-4 flex items-center ">
-      <Link href="/">Home</Link>
-      {parts.map((part, index) => {
-        return (
-          <Link key={index} href={`/${part.toLowerCase()}`}>
-            {part}
-          </Link>
-        );
-      })}
+      {breadcrumbs.map((crumb, index) => (
+        <Link key={index} href={crumb.href}>
+          {index + 1 === breadcrumbs.length ? ( // Last item - don't add ">"
+            crumb.title
+          ) : (
+            <>
+              {crumb.title} {'>'}
+            </>
+          )}
+        </Link>
+      ))}
     </nav>
   );
 };
