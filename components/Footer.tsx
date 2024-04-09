8ed3cfecbed0b171375f09d753bab06eb6a13889
Florian <floryus2004@gmail.com>
2024-04-09 10:42:10 +0200

    merge conflicts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,15 @@
 import Link from "next/link";
 
 const Footer = () => {
-
-    return(
-        <footer className="flex justify-between">
-            <Link href="https://github.com/Floryus/chess-tactics">
-                <span className="emoji">👾</span> GitHub
-            </Link>
-            <Link href="/Impressum">
-                <span className="emoji">📩</span> Impressum
-            </Link>
-        </footer>
-    )
-
-}
+  return (
+    <footer className="flex justify-between">
+      <Link href="https://github.com/Floryus/chess-tactics">
+        <span className="emoji">👾</span> GitHub
+      </Link>
+      <Link href="/imprint">
+        <span className="emoji">📩</span> Imprint
+      </Link>
+    </footer>
+  );
+};
 export default Footer;
--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
   // publicRoutes: ['/anyone-can-visit-this-route'],
-  publicRoutes: ["/", "/login"],
+  publicRoutes: ["/", "/login", "/imprint"],
   // Prevent the specified routes from accessing
   // authentication information:
   // ignoredRoutes: ['/no-auth-in-this-route'],
